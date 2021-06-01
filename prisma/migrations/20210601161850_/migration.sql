b34d4753784d7636a3a0e32a93ede5cd9a60ebf7
Alexander Zakharov <alzak@envisage.no>
2021-06-01 18:24:44 +0200

    feat: add prisma

---
+++ prisma/migrations/20210601161850_/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
