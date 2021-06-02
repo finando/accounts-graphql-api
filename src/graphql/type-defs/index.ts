2b3418ea839914c78f9cd7f031d7846c4b30bbc1
Alexander Zakharov <alzak@envisage.no>
2021-06-02 08:15:22 +0200

    feat: extend Account model

--- src/graphql/type-defs/index.ts
+++ src/graphql/type-defs/index.ts
@@ -1,9 +1,11 @@
 import { gql } from 'apollo-server';
 
+import enums from './enums';
 import types from './types';
 import inputs from './inputs';
 
 export default gql`
+  ${enums}
   ${types}
   ${inputs}
 
--- src/graphql/type-defs/types.ts
+++ src/graphql/type-defs/types.ts
@@ -3,5 +3,8 @@ import { gql } from 'apollo-server';
 export default gql`
   type Account @key(fields: "id") {
     id: ID!
+    name: String
+    balance: Float!
+    currency: Currency!
   }
 `;
