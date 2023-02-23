28f9d5b14be05f4e51a65c28357e2c578be86cad
Alexander Zakharov <alexander.zakharov@finando.app>
2023-02-23 01:36:07 +0100

    refactor(project): remove redundant server.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
 import { NODE_ENV as environment, HOST as host, PORT as port } from '@app/env';
+import { type Context } from '@app/types';
 import logger, { serverTags } from '@app/utils/logging';
 
-import server from './server';
+import context from './graphql/context';
+import plugins from './graphql/plugins';
+import schema from './graphql/schema';
 
-server({ host, port: Number(port) }).then(({ url }) => {
+startStandaloneServer(new ApolloServer<Context>({ schema, logger, plugins }), {
+  context,
+  listen: { host, port: Number(port) },
+}).then(({ url }) => {
   logger.info(`Application is running at ${url} in ${environment} mode`, {
     tags: [...serverTags, 'start'],
   });
